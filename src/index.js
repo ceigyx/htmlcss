f3b6a38c3623910cb16a80513635d5a6e4dc46b2
ceigyx <aklypse_ceigyx@live.ca>
2020-07-31 04:34:20 -0400

    added reset call to submit form action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('loaded');
-document.querySelector('form').addEventListener(
-    'submit', (event) => {
-        event.preventDefault();
-    }
-)
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('submitted');
+  document.querySelector('form').reset();
+});
