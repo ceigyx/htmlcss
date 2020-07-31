c0e979d8544c70d0cfc90ac9d333e8e9ff648c00
ceigyx <aklypse_ceigyx@live.ca>
2020-07-30 21:53:06 -0400

    set up webpack with devserver & babel

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public', 'assets', 'scripts'),
+    publicPath: '/assets/scripts/',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      verbose: true,
+    }),
+  ],
+};
