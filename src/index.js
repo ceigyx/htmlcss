5a4dc9cab8d1f95674c67efd8226e354e98a5753
ceigyx <aklypse_ceigyx@live.ca>
2020-08-10 18:25:20 -0400

    added lightbox 2 for cases images, completed all website pages, added smooth scrolling
    image assets move/ranamed from img/* to images/* for better compatibility

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+import $ from 'jquery'
+import 'lightbox2'
+
 console.log('loaded');
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove('top');
+    if (!scrolled) {
+      navbar.style.transform = 'translateY(-70px)';
+    }
+    setTimeout(() => {
+      navbar.style.transform = 'translateY(0)';
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add('top');
+    scrolled = false;
+  }
+};
+
+$(document).ready(function () {
+  $('#navbar a, .btn').on('click', function (e) {
+    if (this.hash !== '') {
+      const hash = this.hash;
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100,
+        },
+        800
+      );
+    }
+  });
+});
