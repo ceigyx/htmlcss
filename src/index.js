794ad0a99027c577f19f438a5fd8e8b691a4b259
ceigyx <aklypse_ceigyx@live.ca>
2020-08-14 17:57:13 -0400

    completed timeline sample

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log('loaded');
+
+const items = document.querySelectorAll('#timeline li');
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.height <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () => {
+  items.forEach((item) => {
+    if (isInViewport(item)) {
+      item.classList.add('show');
+    }
+  });
+};
+
+// Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
