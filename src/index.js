c98b482ef8d48b6375b97efbf5d53d19eedb7e8b
ceigyx <aklypse_ceigyx@live.ca>
2020-08-15 20:38:25 -0400

    initial prototype soundboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('loaded');
+console.log('hello');
+
+
+for (const element of document.getElementsByTagName('button')) {
+    element.addEventListener('click', (e) => {
+        e.target.nextElementSibling.play();    
+    })
+
+    element.nextElementSibling.onplay = function() {
+        console.log(element, this)
+        this.previousElementSibling.style.backgroundColor = 'red';
+    }
+
+    element.nextElementSibling.onended = function() {
+        console.log(element, this)
+        this.previousElementSibling.style.backgroundColor = '#333';
+    }
+}
